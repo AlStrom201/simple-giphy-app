a93ff6ae537946bbe3d92fda338f261ceb95a480
AlStrom201 <keepostrom@gmail.com>
2019-08-30 14:45:50 +0800

    Minor Fixes

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,6 @@ function js() {
   return src('script.js').pipe(dest('dist/'));
 }
 
-function img() {
-  return src('images/*.gif').pipe(dest('dist/images'));
-}
-
 exports.js = js;
 exports.html = html;
 exports.default = parallel(html, js);
